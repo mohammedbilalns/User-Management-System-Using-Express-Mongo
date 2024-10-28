ca18e761bc5140f7b6c4f052da21f64806197400
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2024-10-28 10:04:33 +0530

    added: new user creation in dashboard

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,7 +1,8 @@
 import adminModel from "../model/adminModel.js";
+import userModel from "../model/userModel.js";
 import { log } from "mercedlogger";
 import bcrypt from "bcrypt"
-import userModel from "../model/userModel.js"
+
 
 const loadLogin = async (req, res)=>{
 
@@ -15,7 +16,7 @@ const loadDashBoard = async (req, res) => {
         if(!admin) return res.render("admin/login")
 
         const  users = await userModel.find({})
-        res.render("admin/dashboard", {users})
+        res.render("admin/dashboard", {users, isAdminLoginned:true })
     }catch(error){
         log.red("ERROR", error)
     }
@@ -42,6 +43,25 @@ const login = async (req, res)=>{
     }
 }
 
+const logout = (req, res)=>{
+    req.session.admin = null
+    res.render('admin/login')
+}
+
+const addusers = async (req, res)=>{
+    try{
+        const {username, email , password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new userModel({username, email, password: hashedPassword})
 
 
-export default {loadLogin, loadDashBoard, login}
+        await newUser.save()
+
+        res.redirect('/admin/dashboard')
+    }catch(error){
+        log.red("ERROR", error)
+        res.redirect('/admin/dashboard')
+    }
+}
+
+export default {loadLogin, loadDashBoard, login,logout, addusers}
--- routes/admin.js
+++ routes/admin.js
@@ -10,5 +10,7 @@ router.use(express.static('static'))
 router.get('/login',adminAuth.isLogin,adminController.loadLogin )
 router.get('/dashboard',adminAuth.checkSession, adminController.loadDashBoard)
 router.post('/login' ,adminController.login)
+router.get('/logout', adminAuth.checkSession,adminController.logout )
+router.post('/addusers',adminAuth.checkSession, adminController.addusers )
 
 export default router
